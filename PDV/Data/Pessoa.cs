8a5aa2da22ad6df4c5eaba8d74eeb1e81a31a085
AJFNC <ajfnc@hotmail.com>
2023-12-11 12:36:52 -0300

    Implantado o Estoque e Cores

---
+++ PDV/Data/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿namespace PDV.Data
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string? Categoria { get; set; }
+        public string? CPF { get; set; }
+        public string? Funcao { get; set; }
+        public string? Endereco { get; set; }
+        public string? Celular { get; set; }
+
+        public string? Data { get; set; } = DateTime.Now.ToLocalTime().ToString();
+    }
+}
